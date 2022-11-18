164ee97d4ba11297d3c9381121c80fc5065f5096
Ricardo Braga <rcardiobraga@gmail.com>
2022-11-18 12:23:02 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -9,7 +10,9 @@ type Props = {
 
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
-        .then(res => console.log("Sucesso"))
+        .then(res => {
+            toast.info("SMS enviado com sucesso!")
+        })
         
 }
 
